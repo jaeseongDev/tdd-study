7e5d3ab6225333919b87a763945a8a40c79a5ba7
jaeseongDev <qkrwotjd1445@naver.com>
2021-01-03 20:12:02 +0900

    1장 : 다중 통화를 지원하는 Money 객체

--- src/main/java/Dollar.java
+++ src/main/java/Dollar.java
@@ -1,11 +1,11 @@
 public class Dollar {
-    int amount = 10;
+    int amount;
 
     Dollar(int amount) {
-
+        this.amount = amount;
     }
 
     void times(int multiplier) {
-
+        amount *= multiplier;
     }
 }
