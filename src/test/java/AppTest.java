00217848ab8a9cfa0534785711a9edaa8f158651
jaeseongDev <qkrwotjd1445@naver.com>
2021-01-04 12:07:37 +0900

    3장 : 모두를 위한 평등

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -1,4 +1,6 @@
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
 
@@ -10,5 +12,11 @@ public class AppTest {
         assertEquals(10, product.amount);
         product = five.times(3);
         assertEquals(15, product.amount);
+    }
+
+    @Test
+    public void testEquality() {
+        assertTrue(new Dollar(5).equals(new Dollar(5)));
+        assertFalse(new Dollar(5).equals(new Dollar(6)));
     }
 }
