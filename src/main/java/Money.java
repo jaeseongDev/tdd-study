68c964182c8d2eef25ba28946225684f308205fb
jaeseongDev <qkrwotjd1445@naver.com>
2021-01-04 13:50:54 +0900

    9장 : 우리가 사는 시간

--- src/main/java/Money.java
+++ src/main/java/Money.java
@@ -1,12 +1,18 @@
 public abstract class Money {
+    protected String currency;
     protected int amount;
 
+    Money (int amount, String currency) {
+        this.amount = amount;
+        this.currency = currency;
+    }
+
     static Money dollar(int amount) {
-        return new Dollar(amount);
+        return new Dollar(amount, "USD");
     }
 
     static Money franc(int amount) {
-        return new Franc(amount);
+        return new Franc(amount, "CHF");
     }
 
     public boolean equals(Object object) {
@@ -16,4 +22,8 @@ public abstract class Money {
     }
 
     abstract Money times(int multiplier);
+
+    String currency() {
+        return currency;
+    }
 }
